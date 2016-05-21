1b301d8fd0ca899ec2eff468335323624d93e064
Michael S. Huang <mhuang@marinsoftware.com>
2016-05-21 16:02:23 +0800

    update app name

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 
 setup(
     # GETTING-STARTED: set your app name:
-    name='YourAppName',
+    name='Polls Tutorial',
     # GETTING-STARTED: set your app version:
     version='1.0',
     # GETTING-STARTED: set your app description:
     description='OpenShift App',
     # GETTING-STARTED: set author name (your name):
-    author='Your Name',
+    author='Michael Huang',
     # GETTING-STARTED: set author email (your email):
     author_email='example@example.com',
     # GETTING-STARTED: set author url (your url):
